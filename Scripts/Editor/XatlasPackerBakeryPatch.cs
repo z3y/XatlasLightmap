2db757af65cf635ed4aaeb10fb648c8644d23dad
z3y <33181641+z3y@users.noreply.github.com>
2023-03-27 01:10:56 +0200

    fix build errors, check if UNITY_EDITOR

--- Scripts/Editor/XatlasPackerBakeryPatch.cs
+++ Scripts/Editor/XatlasPackerBakeryPatch.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if UNITY_EDITOR
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -64,3 +65,4 @@ namespace z3y
         }
     }
 }
+#endif
